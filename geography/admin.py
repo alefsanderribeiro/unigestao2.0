19ae99ee9373f376fb722b35e16ed0c56e963a2c
Marcos Serra <marcosserra133@gmail.com>
2024-11-06 19:52:48 -0300

    feat: ✨ Modelos adicionados ao admin.py

--- geography/admin.py
+++ geography/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Country, State, Capital, City, Region
 
-# Register your models here.
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation', 'code', 'country')
+    search_fields = ('name',)
+
+@admin.register(Capital)
+class CapitalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state', 'country')
+    search_fields = ('name',)
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state')
+    search_fields = ('name',)
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('name',)

13321aa0f98f88b21b92393fe534d7ccf67e77ed
Marcos Serra <marcosserra133@gmail.com>
2024-11-13 14:13:49 -0300

    chore: 🔧 Atualizar settings.py com novos apps (cbos, bond, configurations) e configurações adicionais do Jazzmin

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,9 @@ INSTALLED_APPS = [
     # My Apps
     'employees',
     'geography',
+    'cbos',
+    'bond',
+    'configurations',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +72,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DOCKER_MODE = True  # Altere para False para usar SQLite
+DOCKER_MODE = False  # Altere para False para usar SQLite
 
 if DOCKER_MODE:
     load_dotenv()
@@ -142,14 +145,15 @@ MEDIA_URL = '/media/'
 THOUSAND_SEPARATOR = '.',
 USE_THOUSAND_SEPARATOR = True
 
+
 JAZZMIN_SETTINGS = {
-    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    # título da janela (Será o valor padrão de current_admin_site.site_title se ausente ou None)
     'site_title': 'SGP',
 
-    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    # Título na tela de login (máximo de 19 caracteres) (padrão é current_admin_site.site_header se ausente ou None)
     'site_header': 'Uni Gestão',
 
-    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absentor None)
+    # Título na marca (máximo de 19 caracteres) (padrão é current_admin_site.site_header se ausente ou None)
     'site_brand': 'Uni Gestão (SGP)',
 
     'icons': {
@@ -159,31 +163,75 @@ JAZZMIN_SETTINGS = {
 
         # App Employees
         'employees.Employee': 'fa-solid fa-address-book',
-        'employees.MaritalStatus': 'fa-solid fa-hand-holding-heart',
-        'employees.Deficiency': 'fa-solid fa-wheelchair',
-        'employess.DegreeInstruction': 'fa-solid fa-file-contract',
-        'employees.Nationality': 'fa-solid fa-earth-americas',
-        'employees.Race': 'fa-solid fa-hands-holding-circle',
+
 
         # App Geography
         'geography.City': 'fa-solid fa-city',
         'geography.State': 'fa-solid fa-compass',
-        'geography.Country': 'fa-solid fa-earth-americas'
+        'geography.Country': 'fa-solid fa-earth-americas',
+
+        # App 'configurations' (exemplo para os modelos que você deseja agrupar)
+        'configurations.AdmissionType': 'fa-solid fa-cogs',  # Adiciona um ícone ao modelo
+        'configurations.HarmfulExposure': 'fa-solid fa-biohazard',  # Exemplo de ícone
+        'configurations.PaymentType': 'fa-solid fa-credit-card',  # Ícone para Tipo de Pagamento
+        'configurations.Bank': 'fa-solid fa-university',  # Ícone para Banco
+        'configurations.AccountType': 'fa-solid fa-wallet',  # Ícone para Tipo de Conta
+        'configurations.PixType': 'fa-solid fa-qrcode',  # Ícone para Tipo de Pix
+        'configurations.MaritalStatus': 'fa-solid fa-hand-holding-heart',
+        'configurations.Deficiency': 'fa-solid fa-wheelchair',
+        'configurations.DegreeInstruction': 'fa-solid fa-file-contract',
+        'configurations.Nationality': 'fa-solid fa-earth-americas',
+        'configurations.Race': 'fa-solid fa-hands-holding-circle',
     },
 
-    # Welcome text on the login screen
+    # Texto de boas-vindas na tela de login
     'welcome_sign': 'Bem-vindo(a) ao Uni Gestão (SGP)',
 
-    # Copyright on the footer
+    # Copyright no rodapé
     'copyright': 'Coding Solutions LTDA',
 
-    # List of model admins to search from the search bar, search bar omitted if excluded
-    # If you want to use a single search field you dont need to use a list, you can use a simple string
+    # Lista de administradores de modelos a serem pesquisados na barra de pesquisa, barra de pesquisa será omitida se excluída
     'search_model': ['employees.Funcionario',],
 
-    # Whether to show the UI customizer on the sidebar
+    # Se deve mostrar o personalizador de UI na barra lateral
     # 'show_ui_builder': True,
+
+    # Ordenação de modelos
+    'order_with_respect_to': [
+        'auth',  # Coloca o app 'auth' no topo
+        'employees',  # Coloca o app 'employees' após 'auth'
+        'bond',
+        'configurations',
+        'configurations.bank',  # Dentro do app 'configurations', ordena os modelos
+        'configurations.accounttype',  # Dentro do app 'configurations', ordena os modelos
+        'configurations.paymenttype',  # Dentro do app 'configurations', ordena os modelos
+        'configurations.pixtype',  # Dentro do app 'configurations', ordena os modelos
+    ],
+
+    # apps não listados
+    # "hide_apps": ['geography',],
+
+    # models não listados
+    "hide_models": ['bond.AdmissionInfo'],
+
+    "custom_links": {
+        "bond": [{
+            # Qualquer nome que você preferir
+            "name": "Gerenciar Vinculo",
+
+            # nome da URL, ex: `admin:index`, URLs relativas, ex: `/admin/index` ou URLs absolutas, ex: `https://domain.com/admin/index`
+            "url": "admin:bond_admissioninfo_changelist",
+
+            # qualquer ícone do Font Awesome, veja a lista aqui https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2 (opcional)
+            "icon": "fas fa-briefcase",
+
+            # uma lista de permissões que o usuário deve ter para ver este link (opcional)
+            # "permissions": ["bond.view_admissioninfo"]     
+        }]
+    },
+
 }
+
 
 JAZZMIN_UI_TWEAKS = {
     "navbar_small_text": False,

972ba923184ff8ddbe847fb55c8f05e31e4f2e14
Marcos Serra <marcosserra133@gmail.com>
2024-11-13 14:25:22 -0300

    refactor: 🔄 Atualização nos models do app cbos para padronização e otimização
    - Reformulado o model  com melhorias no campo de exibição para o admin.
    - Padronização do verbose_name em português para melhorar a consistência.
    - Adição do related_name em  e  para facilitar consultas reversas.
    - Melhorias de estrutura para facilitar futuras implementações na gestão de CBOs e sinônimos de ocupação.

--- cbos/models.py
+++ cbos/models.py
@@ -5,30 +5,30 @@ class CBO(models.Model):
     occupation = models.CharField(max_length=100, verbose_name='Ocupação')
 
     def __str__(self):
-        return f"{self.code} - {self.occupation}"
+        return f'{self.code} - {self.occupation}'
 
     class Meta:
-        verbose_name = "CBO"
-        verbose_name_plural = "CBOs"
+        verbose_name = 'CBO'
+        verbose_name_plural = 'CBOs'
 
 class Occupation(models.Model):
     cbo = models.ForeignKey(CBO, related_name='occupations', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, verbose_name="Nome da Ocupação")
+    name = models.CharField(max_length=100, verbose_name='Nome da Ocupação')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Ocupação"
-        verbose_name_plural = "Ocupações"
+        verbose_name = 'Ocupação'
+        verbose_name_plural = 'Ocupações'
 
 class Synonym(models.Model):
     occupation = models.ForeignKey(Occupation, related_name='synonyms', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100, verbose_name="Sinônimo")
+    name = models.CharField(max_length=100, verbose_name='Sinônimo')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "Sinônimo"
-        verbose_name_plural = "Sinônimos"
+        verbose_name = 'Sinônimo'
+        verbose_name_plural = 'Sinônimos'

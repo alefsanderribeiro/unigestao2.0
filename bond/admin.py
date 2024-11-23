6c289d15eb195c5d9f74b69ebea840d7ea2b8409
Marcos Serra <marcosserra133@gmail.com>
2024-11-22 22:34:32 -0300

    fix: 🐛 Corrigido o campo de busca para 'full_name' no Admin de AdmissionInfo
    - Alterado 'employee__name' para 'employee__full_name' no campo de busca.

--- bond/admin.py
+++ bond/admin.py
@@ -5,6 +5,6 @@ from .models import AdmissionInfo
 @admin.register(AdmissionInfo)
 class AdmissionInfoAdmin(admin.ModelAdmin):
     list_display = ('employee', 'admission_date', 'salary_value', 'bank', 'agency', 'account_number', 'pix')
-    search_fields = ('employee__name', 'admission_date', 'salary_value')
+    search_fields = ('employee__full_name', 'admission_date', 'salary_value')
     list_filter = ('admission_date', 'payment_type', 'bank', 'account_type')
     ordering = ('admission_date',)

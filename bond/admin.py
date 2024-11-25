fb6012f621ee4943146916c1c968fe7b9b291336
Marcos Serra <marcosserra133@gmail.com>
2024-11-25 16:49:04 -0300

    feat: ✨ Ajuste na configuração do admin de AdmissionInfo
    - Adicionado o campo 'position_01' à exibição da lista (`list_display`).
    - Adicionado o filtro 'cbo' e ajustados os filtros para incluir 'account_type' e 'payment_type' na lista de filtros (`list_filter`).
    - Mantida a ordenação por 'admission_date'.

--- bond/admin.py
+++ bond/admin.py
@@ -4,7 +4,14 @@ from .models import AdmissionInfo
 
 @admin.register(AdmissionInfo)
 class AdmissionInfoAdmin(admin.ModelAdmin):
-    list_display = ('employee', 'admission_date', 'salary_value', 'bank', 'agency', 'account_number', 'pix')
-    search_fields = ('employee__full_name', 'admission_date', 'salary_value')
-    list_filter = ('admission_date', 'payment_type', 'bank', 'account_type')
+    list_display = (
+        'employee', 'position_01', 'admission_date', 'salary_value',
+        'bank', 'agency', 'account_number', 'pix'
+    )
+    search_fields = (
+        'employee__full_name', 'admission_date', 'salary_value'
+    )
+    list_filter = (
+        'admission_date', 'cbo', 'account_type', 'payment_type'
+    )
     ordering = ('admission_date',)

411acafbecc958b0db02a2adb66abe128800df99
Marcos Serra <marcosserra133@gmail.com>
2025-02-18 20:44:56 -0300

    refactor: 🗑️ Remove rotas obsoletas do arquivo de URLs de funcionários

--- employees/urls.py
+++ employees/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from .views import EmployeeListView, EmployeeCreateView, EmployeeUpdateView
 
 urlpatterns = [
-    path('employee_list/', EmployeeListView.as_view(), name='employee-list'),
-    path('employee_create/', EmployeeCreateView.as_view(), name='employee-create'),
-    path('employee_update/<int:pk>/', EmployeeUpdateView.as_view(), name='employee-update'),
+    ...
 ]

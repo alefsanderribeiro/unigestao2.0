e122e33ae2c2a8c805ac1bffbb36e53f31e772f5
Marcos Serra <marcosserra133@gmail.com>
2025-02-18 20:40:14 -0300

    refactor: 🗑️ Adiciona comentários para placeholders nos campos do formulário de funcionário

--- employees/forms.py
+++ employees/forms.py
@@ -4,7 +4,14 @@ from django import forms
 class EmployeeFormAdmin(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(EmployeeFormAdmin, self).__init__(*args, **kwargs)
+        # Classes
         self.fields['cpf'].widget.attrs['class'] = 'mask-cpf'
         self.fields['telephone'].widget.attrs['class'] = 'mask-telephone'
         self.fields['cep'].widget.attrs['class'] = 'mask-cep'
-        self.fields['contact'].widget.attrs['placeholder'] = 'Nome do contato'
+
+        # Placeholder
+        # self.fields['full_name'].widget.attrs['placeholder'] = 'Nome Completo do Funcionário'
+        # self.fields['mother_name'].widget.attrs['placeholder'] = 'Nome Completo da Mãe'
+        # self.fields['father_name'].widget.attrs['placeholder'] = 'Nome Completo do Pai'
+        # self.fields['contact'].widget.attrs['placeholder'] = 'Nome do Contato'
+        # self.fields['telephone'].widget.attrs['placeholder'] = 'Telefone Para Contato'

f0a603cedebc9737094b67cf0f637735acdf1449
Marcos Serra <marcosserra133@gmail.com>
2025-02-22 22:46:20 -0300

    fix: 🛠  Corrigido erro 404 ao acessar arquivos de mídia no Django Admin com Jazzmin
    ### Problema
    Ao tentar acessar arquivos de mídia através do Django Admin com Jazzmin, o sistema retornava um erro 404, indicando que o arquivo não foi encontrado. Esse erro só ocorria quando a aplicação estava rodando fora do Docker.
    
    ### Solução
    - Adicionada configuração para servir corretamente os arquivos de mídia com o Django, utilizando  no arquivo .
    - Agora, os arquivos de mídia podem ser acessados corretamente, independentemente do ambiente de execução.
    
    Closes #4

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+        ) + [
     path('', admin.site.urls),
     # path('', include('employees.urls')),
 ]

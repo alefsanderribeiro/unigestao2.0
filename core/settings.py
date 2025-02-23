adf0e2eeb31c3e9aa1b2d538a159996a127945cc
Marcos Serra <marcosserra133@gmail.com>
2025-02-23 16:12:00 -0300

    feat: ✨ Implementada exclusão automática de arquivos com django-cleanup
    ### Descrição da Melhoria
    Atualmente, arquivos excluídos ou modificados no sistema permanecem armazenados, ocupando espaço desnecessário e aumentando o uso de memória. A proposta é utilizar o pacote django-cleanup para excluir automaticamente arquivos que foram removidos ou substituídos, liberando espaço e otimizando o armazenamento.
    
    ### Justificativa
    A melhoria é importante pois ajuda a manter o sistema mais limpo e eficiente ao reduzir o acúmulo de arquivos desnecessários. Arquivos excluídos ou modificados não ocupando espaço também traz benefícios em termos de desempenho, além de reduzir o risco de erros e desorganização do sistema de arquivos. Isso pode impactar positivamente a equipe de desenvolvimento, pois diminui a necessidade de monitoramento e limpeza manual dos arquivos do sistema.
    
    ### Objetivo
    Implementar a exclusão automática de arquivos através do uso do django-cleanup, que irá:
    - Excluir automaticamente arquivos que são removidos ou alterados no sistema.
    - Liberar espaço de armazenamento e evitar a sobrecarga de memória com arquivos obsoletos.
    - Automatizar a manutenção do sistema de arquivos, tornando a aplicação mais eficiente.
    
    Closes #5

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup.apps.CleanupConfig',
 
     # My Apps
     'employees',
